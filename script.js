091e85065bfb39e0ea9f06a7de6d4f53cf7f9556
uka-enkbold <uuganchimeg1104@gmail.com>
2022-07-21 20:03:19 -0500

    sorted through most to least

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 // TODO: add code here
-window.addEventListener('load', function() {
-    
-fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      console.log(json);
-      const container = document.querySelector('#container');
-      let astronauts = '';
-      window.alert(0);
-      for (astronaut of json) {
-        astronauts += `
-        <div class="astronaut">
-          <div class="bio">
-            <h3>${astronaut.firstName} ${astronaut.lastName} </h3>
-            <ul>
-            <li>Hours in space: ${astronaut.hoursInSpace}</li>
-            <li>Active: ${astronaut.active}</li>
-            <li>Skills: ${astronaut.skills}</li>
-            </ul>
-            </div>
-        <img class="avatar" src="${astronaut.picture}">
+window.addEventListener('load', () => {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then((response) => {
+      response.json().then( (json) => {
+          const div = document.getElementById('container')
+          let sortedAstronauts = json.sort((a, b) => parseFloat(b.hoursInSpace) - parseFloat(a.hoursInSpace))
+          let astronauts = ''
+          for (let i = 0;i<sortedAstronauts.length; i++) {
+              astronauts += 
+      `<div class='astronaut'>
+        <div class='bio'>
+          <h3>${sortedAstronauts[i].firstName} ${sortedAstronauts[i].lastName}</h3>
+          <ul>
+              <li>Hours in Space: ${sortedAstronauts[i].hoursInSpace}</li>
+              <li>Active: ${sortedAstronauts[i].active}</li>
+              <li>Skills: ${sortedAstronauts[i].skills}</li>
+          </ul>
         </div>
-        `;
-      }
-      container.innerHTML = astronauts;
-    });
-  
-  });
+        <img class='avatar' src=${sortedAstronauts[i].picture}><img/>
+      </div>
+          `}
+          
+          div.innerHTML = astronauts
+      })
+  })
+})
